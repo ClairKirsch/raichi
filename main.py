568854ce5bd7db95371664a4ec0c2bc07ad85d7c
Clair Kirsch <231586689+ClairKirsch@users.noreply.github.com>
2025-12-01 20:52:02 -0500

    refactor: implement lifespan context manager for database setup

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
 
 from dependencies.db import create_db_and_tables
 from routers.token import router as token_router
 from routers.user import router as user_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(
     prefix="",
@@ -14,7 +20,3 @@ app.include_router(
     router=user_router
 )
 
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
