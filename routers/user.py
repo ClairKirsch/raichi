4e05b8c99ca3469a90ee78f754e6e3b68c90e1e3
Clair Kirsch <231586689+ClairKirsch@users.noreply.github.com>
2025-12-01 20:52:56 -0500

    add user creation endpoint

--- routers/user.py
+++ routers/user.py
@@ -6,7 +6,16 @@ from sqlmodel import Session, select
 from dependencies.auth import get_current_active_user
 
 from dependencies.db import get_session
-from dependencies.users import User, UserInfo
+from dependencies.users import User, UserCreate, UserInfo
+from pwdlib import PasswordHash
+import re
+
+argon2i_mcf_regex = re.compile(
+    r'^\$argon2id\$v=\d+\$m=\d+,t=\d+,p=\d+\$[A-Za-z0-9+/]+\$[A-Za-z0-9+/]+={0,2}$'
+)
+
+def is_argon2i_mcf(hash_str: str) -> bool:
+    return bool(argon2i_mcf_regex.match(hash_str))
 
 router = APIRouter(tags=["user information"], prefix="/users")
 
@@ -25,3 +34,29 @@ async def read_user_by_id(
     if not user_data:
         raise HTTPException(status_code=404, detail="User not found")
     return user_data
+
+@router.post("/", response_model=UserInfo, summary = "Create a new user", description = "Create a new user with the provided information.")
+async def create_user(
+    user: UserCreate,
+    session: Annotated[Session, Depends(get_session)],
+):
+    if is_argon2i_mcf(user.password):
+          pass
+    else:
+        user.password = PasswordHash.recommended().hash(user.password)
+
+    existing_user = session.exec(select(User).where(User.username == user.username)).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        full_name=user.full_name,
+        hashed_password=user.password,  # In a real app, hash the password!
+        bio=user.bio,
+        profile_image=user.profile_image,
+    )
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return new_user
