574e0654684d38a394d8839d03824c4af84fa09b
Clair Kirsch <231586689+ClairKirsch@users.noreply.github.com>
2025-12-09 11:06:07 -0500

    Fix unit test

--- test_main.py
+++ test_main.py
@@ -4,8 +4,13 @@ client = testclient.TestClient(app)
 
 jwt_token = ""
 
-def test_read_venue_not_found():
-    response = client.get("/venues/9999")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Venue not found"}
+from fastapi.testclient import TestClient
+from main import app
 
+jwt_token = ""
+
+def test_read_venue_not_found():
+    with TestClient(app) as client:
+        response = client.get("/venues/9999")
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Venue not found"}
