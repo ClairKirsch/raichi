0ec82703b4dd8fbc59eea83c5c72c986d61e1c98
Clair Kirsch <231586689+ClairKirsch@users.noreply.github.com>
2025-12-08 21:58:17 -0500

    Add API endpoints to public API

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from fastapi.concurrency import asynccontextmanager
 from dependencies.db import create_db_and_tables
 from routers.token import router as token_router
 from routers.user import router as user_router
+from routers.message import router as message_router
+from routers.venue import router as venue_router
+from routers.events import router as events_router
 
 
 @asynccontextmanager
@@ -19,3 +22,6 @@ app.include_router(
     router=token_router,
 )
 app.include_router(router=user_router)
+app.include_router(router=message_router)
+app.include_router(router=venue_router)
+app.include_router(router=events_router)
