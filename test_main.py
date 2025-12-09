180fc4020858b68820faf37b18cb899f196f17ef
Clair Kirsch <231586689+ClairKirsch@users.noreply.github.com>
2025-12-09 15:29:28 -0500

    Actual unit tests

--- test_main.py
+++ test_main.py
@@ -1,10 +1,49 @@
 from fastapi.testclient import TestClient
 from main import app
+import os.path
 
 jwt_token = ""
 
-def test_read_venue_not_found():
+def test_user_registration():
+    if os.path.exists("database.db"): # Hack until this is moved to Postgres
+        os.remove("database.db")
+    
     with TestClient(app) as client:
-        response = client.get("/venues/9999")
-        assert response.status_code == 404
-        assert response.json() == {"detail": "Venue not found"}
+        response = client.post(
+            "/users/",
+            json={
+                "username": "testuser",
+                "password": "testpassword"
+            }
+        )
+        assert response.status_code == 200
+
+def test_user_login():
+    global jwt_token
+    with TestClient(app) as client:
+        response = client.post(
+            "/token",
+            data={
+                "username": "testuser",
+                "password": "testpassword",
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+        assert response.status_code == 200
+        jwt_token = response.json().get("access_token")
+        assert jwt_token is not None
+
+def test_create_venue():
+    with TestClient(app) as client:
+        response = client.post(
+            "/venues/",
+            json={
+                "name": "Test Venue",
+                "address": "123 Test St",
+                "latitude": 40.7128,
+                "longitude": -74.0060
+            },
+            headers={"Authorization": f"Bearer {jwt_token}"}
+        )
+        print(response.json())
+        assert response.status_code == 200
